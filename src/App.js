1bf7ed9944079303604197c9e28f973472818e49
Nikhil Menghani <nikhil@menghani.com>
2023-07-12 22:02:35 -0400

    Add a menubar on the website

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Menu } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
@@ -7,10 +9,31 @@ import Contact from './Contact';
 function App() {
   return (
     <Router>
+      <Menu pointing secondary>
+        <Menu.Item
+          name='home'
+          active={window.location.pathname === '/'}
+        >
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item
+          name='about'
+          active={window.location.pathname === '/about'}
+        >
+          <Link to="/about">About</Link>
+        </Menu.Item>
+        <Menu.Item
+          name='contact'
+          active={window.location.pathname === '/contact'}
+        >
+          <Link to="/contact">Contact</Link>
+        </Menu.Item>
+      </Menu>
+
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/web" element={<Home />} />
       </Routes>
     </Router>
   );
