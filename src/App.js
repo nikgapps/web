ed1e2f899173811501489c8f4cc11b841e44733e
Nikhil Menghani <nikhil@menghani.com>
2023-07-12 21:39:24 -0400

    Update the webpage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button primary>Click Here</Button>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
